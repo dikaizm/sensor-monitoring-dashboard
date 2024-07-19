c9f9a3eff46a8ba495adc88b645b066846e402e8
Dika <dikamaah@gmail.com>
2024-07-20 02:05:52 +0700

    Backend only branch

--- frontend/src/config/env.ts
+++
@@ -1,6 +0,0 @@
-const appConfig = {
-    apiUrl: import.meta.env.VITE_API_URL as string,
-    wsUrl: import.meta.env.VITE_WS_URL as string,
-}
-
-export default appConfig
--- frontend/src/types/constant.ts
+++
@@ -1,5 +0,0 @@
-export const InitialWindowOpen: WindowOpenType = {
-    camera: false,
-    photoelectric: false,
-    conveyor: false
-}
